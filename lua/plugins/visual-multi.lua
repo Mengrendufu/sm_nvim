82dda4daf79ccfe44faab18b9c65774d63361678
Mengrendufu <mengrendufu@163.com>
2026-03-06 00:08:45 +0800

    feat(plugin): add mg979/vim-visual-multi multiple-cursor plugin via new plugin file

---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,9 @@
+-- 多光标编辑插件：mg979/vim-visual-multi
+return {
+  "mg979/vim-visual-multi",
+  branch = "master",
+  init = function()
+    vim.g.VM_default_mappings = 1
+    -- 如需自定义 keymap，可补充 vim.g.VM_maps
+  end,
+}
