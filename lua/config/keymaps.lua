8497166a1cd976698235b9f3249e00d73a869fc2
Mengrendufu <mengrendufu@163.com>
2026-03-07 00:33:20 +0800

    feat: visual substitute scoped to selection via :'<,'>s/

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,8 +24,25 @@ local function Substitute()
     )
 end
 
--- 映射快捷键 (使用 <leader>s 以匹配 Substitute 的首字母)
+local function SubstituteVisual()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'x', false)
+    local start = vim.fn.getpos("'<")
+    local finish = vim.fn.getpos("'>")
+    local lines = vim.api.nvim_buf_get_text(0, start[2] - 1, start[3] - 1, finish[2] - 1, finish[3], {})
+    local target = table.concat(lines, '\n')
+    if target == '' then
+        vim.notify('未选中任何内容', vim.log.levels.WARN)
+        return
+    end
+    local escaped = vim.fn.escape(target, '/\\')
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes(":'<,'>s/" .. escaped .. '/', true, false, true),
+        'n', false
+    )
+end
+
 vim.keymap.set('n', '<leader>es', Substitute, { desc = '交互式全局替换当前单词' })
+vim.keymap.set('v', '<leader>es', SubstituteVisual, { desc = '交互式全局替换选中内容' })
 
 -- 快速保存
 vim.keymap.set('n', '<leader>fs', ':w<CR>', { desc = '保存文件', silent = true })
