d8dfbbff70a66fd347544c1d3ccf0dff4c43cd41
Mengrendufu <mengrendufu@163.com>
2026-04-29 17:02:40 +0800

    Fix Treesitter config for Neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,15 +5,12 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",  -- ← 使用旧的稳定分支
+        branch = "main",
         build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
+        lazy = false,
 
         config = function()
-        require('nvim-treesitter.install').prefer_git = true
-        require("nvim-treesitter.configs").setup({
-            -- 安装的语言
-            ensure_installed = {
+            local languages = {
                 -- 核心语言
                 "c", "cpp", "lua", "vim", "vimdoc", "query",
                 -- 脚本语言
@@ -22,38 +19,21 @@ return {
                 "json", "toml",
                 -- 系统编程
                 "rust",
-            },
+            }
 
-            -- 安装选项
-            sync_install = false,      -- 同步安装（默认false，异步安装）
-            ignore_install = { "latex" },       -- 要忽略安装的语言列表
-            modules = {},              -- 自定义模块配置
+            require("nvim-treesitter").setup()
 
-            -- 自动安装
-            auto_install = true,
+            local missing = require("nvim-treesitter.config").norm_languages(languages, { installed = true })
+            if #missing > 0 then
+                require("nvim-treesitter").install(missing)
+            end
 
-            -- 语法高亮
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            -- 增量选择
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "<CR>",
-                    scope_incremental = "<S-CR>",
-                    node_decremental = "<BS>",
-                },
-            },
-
-            -- 智能缩进
-            indent = {
-                enable = true,
-            },
-        })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = languages,
+                callback = function()
+                    vim.treesitter.start()
+                end,
+            })
         end,
     },
 }
